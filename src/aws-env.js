7116860a00d98e6607352f5ce2d669c4ab4edd9e
tugacomum <internetsoundseasy@gmail.com>
2024-02-08 21:47:32 +0000

    teste

---
+++ src/aws-env.js
@@ -0,0 +1,7 @@
+import 'dotenv/config';
+
+export default {
+    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+    AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
+    AWS_DEFAULT_REGION: process.env.AWS_DEFAULT_REGION
+}
